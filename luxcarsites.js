89771f8f67dcd1f399f920e76769502bfe3674a2
JoAnn Ciaramello <joannciaramello@gmail.com>
2021-10-20 18:14:28 -0400

    Add files via upload
    Initial Commit

---
+++ luxcarsites.js
@@ -0,0 +1,34 @@
+export const LUXCARSITES = [
+    {
+        id: 0,
+        name: "Aston Martin",
+        image: "/assets/images/2021-aston-martin-vantage-roadster.jpg",
+        elevation: 1233,
+        featured: false,
+        description: "Lorem Ipsum."
+    },
+    {
+        id: 1,
+        name:"Ferrari",
+        image: "/assets/images/2020-ferrari-f8-spyder.jpg",
+        elevation: 877,
+        featured: false,
+        description: "Lorem Ipsum."
+    },
+    {
+        id: 2,
+        name:"Lamborghini",
+        image: "/assets/images/2021-lamborghini-huracan-evo.jpg",
+        elevation: 2901,
+        featured: false,
+        description: "Lorem Ipsum."
+    },
+    {
+        id: 3,
+        name:"Mclaren",
+        image: "/assets/images/mclaren-elva.jpg",
+        elevation: 42,
+        featured: true,
+        description: "Lorem Ipsum."
+    }
+];
