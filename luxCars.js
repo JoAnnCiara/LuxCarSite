8205ab9d65c085889c54f632042c14ba3e0f6f0f
JoAnn Ciaramello <joannciaramello@gmail.com>
2021-10-26 15:46:32 -0400

    Add files via upload
    Added file with car information and images

---
+++ luxCars.js
@@ -0,0 +1,30 @@
+export const LUXCARS = [
+    {
+        id: 0,
+        name: 'Aston Martin',
+        image: 'assets/images/redaston.jpg',
+        elevation: 1233,
+        description: "Lorem Ipsum."
+    },
+    {
+        id: 1,
+        name: 'Mclaren ',
+        image: 'assets/images/mclaren.jpg',
+        elevation: 877,
+        description: "Lorem Ipsum."
+    },
+    {
+        id: 2,
+        name: 'Ferrari',
+        image: 'assets/images/ferrari.jpg',
+        elevation: 2901,
+        description: "Lorem Ipsum."
+    },
+    {
+        id: 3,
+        name: 'Lamborghini',
+        image: 'assets/images/lambo.jpg',
+        elevation: 42,
+        description: "Lorem Ipsum."
+    }
+];
