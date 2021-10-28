961e95db20748cd3dbe4bdf539aea03bd58aa857
JoAnn Ciaramello <joannciaramello@gmail.com>
2021-10-28 00:04:25 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,37 +1,18 @@
 import React, { Component } from 'react';
-import { Nav, NavLink, NavItem } from 'reactstrap';
-import Directory from './components/DirectoryComponent';
-import { LUXCARS } from './shared/luxCars';
+import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            luxCars: LUXCARS
-        };
-    }
     render() {
         return (
-            <div className="App" >
-                <Nav vertical>
-                    <NavItem>
-                    <NavLink href="#">Home</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="#">Directory</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="#">About Us</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="#">Contact Us</NavLink>
-                    </NavItem>
-                </Nav>
-                <Directory luxCars={this.state.luxCars} />
-            </div>
+            <BrowserRouter>
+                <div className="App">
+                    <Main />
+                </div>
+            </BrowserRouter>
         );
-    }
+    };
 }
 
 export default App;
